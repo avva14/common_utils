98548b185d29a721ec6fbf73d40676dbf3f0c88c
avva14 <avva14@gmail.com>
2023-02-01 21:45:16 +0400

    Fix and comments

--- picutils.py
+++ picutils.py
@@ -1,6 +1,7 @@
-import numpy as np
-
 def get_properbb(img):
+    '''
+    Returns box that contains not-zero values
+    '''
     bycol = np.where(np.squeeze(np.any(img > 0, axis=1)))[0]
     byrow = np.where(np.squeeze(np.any(img > 0, axis=0)))[0]
     ymin, ymax = bycol[0], bycol[-1]
@@ -9,8 +10,14 @@ def get_properbb(img):
     
     
 def intersection(seg1Start, seg1End, seg2Start, seg2End):
+    '''
+    Intersection of two intervals: true if intersection takes place
+    '''
     return seg2Start <= seg1End and seg1Start <= seg2End
 def intersectionp(seg1Start, seg1End, seg2Start, seg2End, period):
+    '''
+    Intersection of two intervals with periodic boundary conditions
+    '''
     l0 = intersection(seg1Start, seg1End, seg2Start, seg2End)
     l1 = intersection(seg1Start, seg1End, seg2Start + period, seg2End + period)
     l2 = intersection(seg1Start, seg1End, seg2Start - period, seg2End - period)
@@ -18,21 +25,30 @@ def intersectionp(seg1Start, seg1End, seg2Start, seg2End, period):
     
     
 def intersectlen(seg1Start, seg1End, seg2Start, seg2End):
+    '''
+    Intersection length of two intervals
+    '''
     if not intersection(seg1Start, seg1End, seg2Start, seg2End):
         return 0
     mn = np.minimum(seg1End, seg2End)
     mx = np.maximum(seg1Start, seg2Start)
     return mn - mx
 def intersectlenp(seg1Start, seg1End, seg2Start, seg2End, period):
+    '''
+    Intersection length of two intervals with periodic boundary conditions
+    '''
     l0 = intersectlen(seg1Start, seg1End, seg2Start, seg2End)
     l1 = intersectlen(seg1Start, seg1End, seg2Start + period, seg2End + period)
     l2 = intersectlen(seg1Start + period, seg1End + period, seg2Start, seg2End)
     return max(l0,l1,l2)
-def compute_iou(box1, box2):
+def compute_iou(box1, box2, period):
+    '''
+    IoU of two boxes with periodic boundary conditions on Y dimension
+    '''
     A1 = box1[2] * box1[3]
     A2 = box2[2] * box2[3]
     P1 = intersectlen(box1[0], box1[0]+box1[2], box2[0], box2[0]+box2[2])
-    P2 = intersectlenp(box1[1], box1[1]+box1[3], box2[1], box2[1]+box2[3], SIZE)
+    P2 = intersectlenp(box1[1], box1[1]+box1[3], box2[1], box2[1]+box2[3], period)
     AA = P1 * P2
     return  AA / min(A1, A2)
     
